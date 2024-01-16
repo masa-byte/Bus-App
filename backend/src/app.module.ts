7601f84a02121c97f0f6fd45498017694d4891d4
ajovano <aleksa.jovanovicc@elfak.rs>
2024-01-16 23:27:05 +0100

    Kesiranje radi.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,14 +8,25 @@ import { MyNeo4jModule } from './my-neo4j/my-neo4j.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { RolesGuard } from './auth/guards/roles.guard';
 import { CompanyModule } from './company/company.module';
 import { TownModule } from './town/town.module';
 import { BusLineModule } from './bus-line/bus-line.module';
+import { CacheInterceptor, CacheModule, CacheStore } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    //CacheModule.register(),
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: async () => ({
+        store: (await redisStore.redisStore({
+          url: 'redis://localhost:6379',
+        })) as unknown as CacheStore,
+      }),
+    }),
     Neo4jModule.forRoot(neo4jConfig),
     RedisModule,
     MyNeo4jModule,
@@ -34,6 +45,10 @@ import { BusLineModule } from './bus-line/bus-line.module';
     {
       provide: APP_GUARD,
       useClass: RolesGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })
